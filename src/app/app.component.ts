6869cb7dbb829b5647762840e5fbd251dd356d7c
Nitish <nitish@nagarro.com>
2019-09-13 11:18:51 +0530

    iframe changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ngOninit() {
+    window.addEventListener("message", this.messageHandler, false);
+  }
+
+  messageHandler(event) {
+    debugger;
+  }
 }
