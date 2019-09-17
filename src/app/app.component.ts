06324daea10f6ede3b38b833ee101ea564ec6caa
Nitish <nitish@nagarro.com>
2019-09-17 11:54:21 +0530

    late loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  shouldLoad: boolean = false;
   ngOninit() {
-    debugger;
-    console.log(localStorage.getItem("abc"));
-    alert(localStorage.getItem("abc"));
-    window.addEventListener("message", this.messageHandler, false);
-    window.postMessage("message","*");
+    window.parent.postMessage({"isChildLoaded": true},"*");
+    const domains = ["http://localhost:3000"];
+    window.addEventListener("message",  (event) => {
+      if(!domains.includes(event.origin)) return;
+      const { action, key, value } = event.data;
+      if (action == "save") {
+        localStorage.setItem(key, value);
+        this.shouldLoad = true;
+      }
+      alert("got message");
+    });
+    // debugger;
+    // console.log(localStorage.getItem("abc"));
+    // alert(localStorage.getItem("abc"));
+    // window.addEventListener("message", this.messageHandler, false);
+    // window.postMessage("message","*");
   }
 
   messageHandler(event) {
